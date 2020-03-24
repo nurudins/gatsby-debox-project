b924f0cf4a4a1f397ee9b7b4f43335296d2036fe
nurudins <nurudin.santoso@ub.ac.id>
2020-03-24 19:48:29 +0700

    Navbar added as well as basic css

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,9 +5,11 @@ import PropTypes from "prop-types"
 
 import "./layout.css"
 import "./bootstrap.min.css"
+import Navbar from './Globals/Navbar'
 
 const Layout = ({ children }) => 
   <> 
+  <Navbar/>
   {children}
   </>;
 
