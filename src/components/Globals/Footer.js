36fba31076700f7dc95f74b43a298cde87a7b9ff
nurudins <nurudin.santoso@ub.ac.id>
2020-03-27 10:16:48 +0700

    footer

--- src/components/Globals/Footer.js
+++ src/components/Globals/Footer.js
@@ -20,10 +20,10 @@ export default class Footer extends Component {
                
                <div className="container">
       <div className="copyright">
-        &copy; Copyright <strong><span>Moderna</span></strong>. All Rights Reserved
+        &copy; Copyright <strong><span>ISEA - DEBOX INDONESIA</span></strong>. All Rights Reserved
       </div>
       <div className="credits">
-         <a href="https://bootstrapmade.com/">BootstrapMade</a>
+         <a href="https://inhuaschool.com/">BootstrapMade</a>
       </div>
     </div>
 </footer>
--- src/pages/services.js
+++ src/pages/services.js
@@ -22,43 +22,83 @@ export default class services extends Component {
             </div>
           </section>
           <div className="container">
+            <div className="row">
+              <div
+                className="col-md-6 col-lg-3 d-flex align-items-stretch"
+                data-aos="fade-up"
+              >
+                <div className="icon-box icon-box-pink">
+                  <div className="icon">
+                    <i className="bx bxl-dribbble"></i>
+                  </div>
+                  <h4 className="title">
+                    <a href="">Lorem Ipsum</a>
+                  </h4>
+                  <p className="description">
+                    Voluptatum deleniti atque corrupti quos dolores et quas
+                    molestias excepturi sint occaecati cupiditate non provident
+                  </p>
+                </div>
+              </div>
 
-        <div className="row">
-          <div className="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="fade-up">
-            <div className="icon-box icon-box-pink">
-              <div className="icon"><i className="bx bxl-dribbble"></i></div>
-              <h4 className="title"><a href="">Lorem Ipsum</a></h4>
-              <p className="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+              <div
+                className="col-md-6 col-lg-3 d-flex align-items-stretch"
+                data-aos="fade-up"
+                data-aos-delay="100"
+              >
+                <div className="icon-box icon-box-cyan">
+                  <div className="icon">
+                    <i className="bx bx-file"></i>
+                  </div>
+                  <h4 className="title">
+                    <a href="">Sed ut perspiciatis</a>
+                  </h4>
+                  <p className="description">
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur
+                  </p>
+                </div>
+              </div>
+
+              <div
+                className="col-md-6 col-lg-3 d-flex align-items-stretch"
+                data-aos="fade-up"
+                data-aos-delay="200"
+              >
+                <div className="icon-box icon-box-green">
+                  <div className="icon">
+                    <i className="bx bx-tachometer"></i>
+                  </div>
+                  <h4 className="title">
+                    <a href="">Magni Dolores</a>
+                  </h4>
+                  <p className="description">
+                    Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum
+                  </p>
+                </div>
+              </div>
+
+              <div
+                className="col-md-6 col-lg-3 d-flex align-items-stretch"
+                data-aos="fade-up"
+                data-aos-delay="200"
+              >
+                <div className="icon-box icon-box-blue">
+                  <div className="icon">
+                    <i className="bx bx-world"></i>
+                  </div>
+                  <h4 className="title">
+                    <a href="">Nemo Enim</a>
+                  </h4>
+                  <p className="description">
+                    At vero eos et accusamus et iusto odio dignissimos ducimus
+                    qui blanditiis praesentium voluptatum deleniti atque
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
-
-          <div className="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
-            <div className="icon-box icon-box-cyan">
-              <div className="icon"><i className="bx bx-file"></i></div>
-              <h4 className="title"><a href="">Sed ut perspiciatis</a></h4>
-              <p className="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-            </div>
-          </div>
-
-          <div className="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
-            <div className="icon-box icon-box-green">
-              <div className="icon"><i className="bx bx-tachometer"></i></div>
-              <h4 className="title"><a href="">Magni Dolores</a></h4>
-              <p className="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-            </div>
-          </div>
-
-          <div className="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
-            <div className="icon-box icon-box-blue">
-              <div className="icon"><i className="bx bx-world"></i></div>
-              <h4 className="title"><a href="">Nemo Enim</a></h4>
-              <p className="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
-            </div>
-          </div>
-
-        </div>
-
-      </div>
         </main>
         <Footer />
       </Layout>
